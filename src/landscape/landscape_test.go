5fa7c046eb324070fa4c9f8616a1c8d6d171dcf6
Mayank <mayankdixit91@gmail.com>
2021-04-03 15:16:06 +0200

    Test Coverage: 100%

--- src/landscape/landscape_test.go
+++ src/landscape/landscape_test.go
@@ -1,16 +1,32 @@
 package landscape
 
 import (
+	"reflect"
 	"testing"
 )
+
+func TestInit(t *testing.T) {
+	for _, testcase := range []struct{
+		obstacles [][]int
+	}{{obstacles: [][]int{{1,1}, {1,2}}}} {
+		landscape := Init(testcase.obstacles);
+		if !reflect.DeepEqual(landscape.Obstacles, testcase.obstacles) {
+			t.Errorf("TC Failed. Obstacle not found in the List. TC: %+v, Landscape: %+v\n", testcase, landscape)
+		} else {
+			t.Logf("Works for testcase: %+v", testcase)
+		}
+	}
+}
 
 func TestLandscape_HasObstacleAt(t *testing.T) {
 	for _, testcase := range []struct{
 		obstacles [][]int
 		testInput []int
-	}{{obstacles: [][]int{{1,1}, {1,2}}, testInput: []int{1,1}}} {
-		landscape := Init(testcase.obstacles);
-		if !landscape.HasObstacleAt(testcase.testInput[0], testcase.testInput[1]) {
+		shouldExist bool
+	}{{obstacles: [][]int{{1,1}, {1,2}}, testInput: []int{1,1}, shouldExist: true},
+		{obstacles: [][]int{{1,1}, {1,2}}, testInput: []int{1,3}, shouldExist: false}} {
+		landscape := Init(testcase.obstacles)
+		if landscape.HasObstacleAt(testcase.testInput[0], testcase.testInput[1]) != testcase.shouldExist {
 			t.Errorf("TC Failed. Obstacle not found in the List. TC: %+v, Landscape: %+v\n", testcase, landscape)
 		} else {
 			t.Logf("Works for testcase: %+v", testcase)
